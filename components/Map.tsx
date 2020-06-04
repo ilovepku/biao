c774b2b8fb55180b855a822c3cddb72672234f90
Sean Lee <seanleecoder@gmail.com>
2020-06-04 23:19:33 +0200

    + BottomSheet

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import { StyleSheet, Dimensions, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import MapView, { Marker } from "react-native-maps";
+import BottomSheet from "reanimated-bottom-sheet";
 import Carousel from "react-native-snap-carousel";
 
 import { InitialRegion, Coordinate, City, Battle } from "../utils/types";
@@ -31,6 +32,7 @@ const Map = ({
 
   const mapRef = useRef<MapView>(null);
   const markerRefs: Array<Marker | null> = [];
+  const bottomSheetRef = useRef<BottomSheet>(null);
   // @ts-ignore: bad practice, needs future fix
   const carouselRef = useRef<Carousel>(null);
 
@@ -44,6 +46,7 @@ const Map = ({
         },
         DEFAULT_ANIMATE_DURATION
       );
+    bottomSheetRef.current && bottomSheetRef.current.snapTo(0);
     carouselRef.current && carouselRef.current.snapToItem(index);
   };
 
@@ -59,6 +62,27 @@ const Map = ({
       );
     markerRefs[index]!.showCallout(); // bad practice, needs future fix
   };
+
+  const bottomSheetContent = () => (
+    <View style={styles.panel}>
+      <Carousel
+        ref={carouselRef}
+        data={battles}
+        renderItem={carouselItem}
+        itemWidth={(width * 3) / 4} // add itemHeight & sliderHeight for vertical carousel in landscape mode
+        sliderWidth={width}
+        onSnapToItem={(index) => onCarouselItemChange(index)}
+      />
+    </View>
+  );
+
+  const bottomSheetHeader = () => (
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle} />
+      </View>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -98,14 +122,16 @@ const Map = ({
         ))}
       </MapView>
 
-      <Carousel
-        ref={carouselRef}
-        containerCustomStyle={styles.carousal}
-        data={battles}
-        renderItem={carouselItem}
-        itemWidth={350} // add itemHeight & sliderHeight for vertical carousel in landscape mode
-        sliderWidth={width}
-        onSnapToItem={(index) => onCarouselItemChange(index)}
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={["30%", "3%", "3%"]}
+        initialSnap={1}
+        renderContent={bottomSheetContent}
+        renderHeader={bottomSheetHeader}
+        enabledContentGestureInteraction={false}
+        onOpenEnd={() =>
+          carouselRef.current.currentIndex === 0 && onCarouselItemChange(0)
+        }
       />
     </View>
   );
@@ -126,9 +152,27 @@ const styles = StyleSheet.create({
       height: 1,
     },
   },
-  carousal: {
-    position: "absolute",
-    bottom: 0,
+  header: {
+    backgroundColor: "#f7f5eee8",
+    shadowColor: "#000000",
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  panelHeader: {
+    alignItems: "center",
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#00000040",
+    marginBottom: 10,
+  },
+  panel: {
+    height: 600,
+    padding: 20,
+    backgroundColor: "#f7f5eee8",
   },
 });
 
