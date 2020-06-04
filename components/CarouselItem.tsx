04cc708a8dd20756d9ce5da21acda1b2ab7f4589
Sean Lee <seanleecoder@gmail.com>
2020-06-04 16:51:08 +0200

    + CarouselItem component and Carousel

---
+++ components/CarouselItem.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as WebBrowser from "expo-web-browser";
+import { Card, Title, Paragraph } from "react-native-paper";
+
+import { Battle } from "../utils/types";
+import { MARKER_ICONS } from "../utils/markerIcons";
+
+const carouselItem = ({
+  item: { title, pinColor, year, type, location, result, link },
+}: {
+  item: Battle;
+}) => {
+  return (
+    <Card>
+      <Card.Title
+        title={title}
+        subtitle={year}
+        left={() => (
+          <MaterialCommunityIcons
+            name={MARKER_ICONS[type]}
+            size={24}
+            color={pinColor}
+          />
+        )}
+        right={() => (
+          <MaterialCommunityIcons
+            name="wikipedia"
+            size={24} 
+            color="black"
+            onPress={() => WebBrowser.openBrowserAsync(link)}
+          />
+        )}
+      />
+      <Card.Content>
+        <Title>{result}</Title>
+        <Paragraph>{location}</Paragraph>
+      </Card.Content>
+      {/* <Card.Cover source={{ uri: "https://picsum.photos/700" }} /> */}
+    </Card>
+  );
+};
+
+export default carouselItem;
--- components/Map.tsx
+++ components/Map.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { StyleSheet, Dimensions, View } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import MapView, { Marker } from "react-native-maps";
+import Carousel from "react-native-snap-carousel";
 
 import { InitialRegion, City, Battle } from "../utils/types";
 import { MARKER_ICONS } from "../utils/markerIcons";
+import carouselItem from "./CarouselItem";
 
 const { width, height } = Dimensions.get("window");
 const aspectRadio = width / height;
@@ -61,6 +63,16 @@ const Map = ({
           </Marker>
         ))}
       </MapView>
+
+      <Carousel
+        // ref={carouselRef}
+        containerCustomStyle={styles.carousal}
+        data={battles}
+        renderItem={carouselItem}
+        itemWidth={350} // add itemHeight & sliderHeight for vertical carousel in landscape mode
+        sliderWidth={width}
+        // onSnapToItem={(index) => onCarouselItemChange(index)}
+      />
     </View>
   );
 };
@@ -79,6 +91,10 @@ const styles = StyleSheet.create({
       width: 1,
       height: 1,
     },
+  },
+  carousal: {
+    position: "absolute",
+    bottom: 0,
   },
 });
 
