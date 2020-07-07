6a37bcd448c8fa5e5517e796f6c0221a3f05556e
Sean Lee <seanleecoder@gmail.com>
2020-07-07 12:06:34 +0200

    * suppress useNativeDriver warnings

--- App.tsx
+++ App.tsx
@@ -6,6 +6,12 @@ import { Spinner } from "native-base";
 import MapScreen from "./components/MapScreen";
 import AboutScreen from "./components/AboutScreen";
 
+// @TODO
+import { YellowBox } from "react-native";
+YellowBox.ignoreWarnings([
+  "Animated: `useNativeDriver` was not specified. This is a required option and must be explicitly set to `true` or `false`",
+]);
+
 export type DrawerParamList = {
   Map: undefined;
   About: undefined;
