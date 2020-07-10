8d53a97e52432dacde63efc485fba9ce5f34ebda
Sean Lee <seanleecoder@gmail.com>
2020-07-10 22:50:07 +0200

    * prepare for deployment

--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,7 @@
-import { compose, createStore } from "redux";
+import { createStore } from "redux";
 
 import rootReducer from "./reducer";
 
-const composeEnhancers =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(rootReducer);
 
-export const store = createStore(rootReducer, composeEnhancers());
-
-export type RootState = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof rootReducer>;
