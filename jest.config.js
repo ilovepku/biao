5637b1367abe6e475e1b546c746654534ddab0c9
Sean Lee <seanleecoder@gmail.com>
2021-02-02 19:16:33 +0100

    + @testing-library/react-native

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  preset: 'react-native',
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)',
+  ],
   collectCoverageFrom: [
     '**/src/**/*.{js,jsx,ts,tsx}',
     '!**/__tests__/**',
